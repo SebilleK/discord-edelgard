17693883b26bb3af42b7d4f4f143c044dced0127
Margarida Grilo <margaridagrilo7@gmail.com>
2024-03-04 22:21:54 +0000

    added embeds

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import { TOKEN } from '../secret/config.js';
 
 // discord.js
-import { Client, IntentsBitField } from 'discord.js';
+import { Client, IntentsBitField, EmbedBuilder } from 'discord.js';
 
 // quotes array
 import { quotes } from './data/quotes.js';
@@ -31,9 +31,7 @@ client.on('messageCreate', message => {
 
 	// if message contains edelgard, default message
 	if (message.content.toLowerCase().includes('edelgard')) {
-		message.reply(
-			'Greetings! 🦅 Your mention of me is honoring. I suggest we focus our efforts on strengthening ourselves, as well as our alliances, if we can. For one of my inspiring quotes, you can type /elquote on the chat. Thank you.',
-		);
+		message.reply(`I see you mentioned me, ${message.author.username}. Thank you for that! Long live the Empire and its ideals. 🦅`);
 	}
 });
 
@@ -46,14 +44,30 @@ client.on('interactionCreate', async interaction => {
 		// if slash command, conditional responses based on name
 	} else {
 		if (interaction.commandName === 'elhey') {
-			interaction.reply('Greetings! 🦅');
+			// using EmbedBuilder (discord.js), you can chain methods to create an embed as you like
+			const embed = new EmbedBuilder()
+				.setColor(0xff0000)
+				.setTitle('Greetings')
+				.setThumbnail('https://assets.fedatamine.com/3h/face_school/12/0.png')
+				.setTimestamp()
+				.addFields({
+					name: 'Hello 🦅!',
+					value: `Thank you for saying hello, ${interaction.user.username}, and I greet you back. Now let us move on to something else.`,
+				});
+			interaction.reply({ embeds: [embed] });
 		}
 
 		if (interaction.commandName === 'elquote') {
 			// pick a random Edelgard quote to reply with
 			// note: already imported json files do not need to be parsed since they already are a js object. currently a js array is being used directly, but this note may be useful in the future.
 			const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-			interaction.reply(`${randomQuote}`);
+
+			const embed = new EmbedBuilder()
+				.setColor(0xff0000)
+				.setThumbnail('https://assets.fedatamine.com/3h/face_school/408/0.png')
+				.setTimestamp()
+				.addFields({ name: 'Edelgard Quote 🦅', value: `${randomQuote}` });
+			interaction.reply({ embeds: [embed] });
 		}
 
 		if (interaction.commandName === 'elsight') {
@@ -63,27 +77,41 @@ client.on('interactionCreate', async interaction => {
 			const userQuestion = interaction.options.getString('question');
 
 			if (!userQuestion) {
+				// failsafe. the question is required though, this should never happen.
 				interaction.reply('I cannot foresee the future without a question. 🦅');
 				return;
 			} else {
+				let reply;
 				try {
 					const response = await predictionApiCall();
-					interaction.reply(`You asked: **"${userQuestion}"**. \nThe 8ball told me: **"${response.reading} "** 🦅`);
+					reply = `You asked: **"${userQuestion}"**. \nThe War Council told me: **"${response.reading} "** 🦅`;
 				} catch (error) {
 					console.error(`Unexpected error while getting response from the API: ${error}`);
-					interaction.reply('Apologies, but an error occurred while foreseeing the future. Please try again later. 🦅');
+					reply = 'Apologies, but an error occurred while foreseeing the future. Please try again later. 🦅';
 				}
+				const embed = new EmbedBuilder()
+					.setColor(0xff0000)
+					.setTimestamp()
+					.setThumbnail('https://assets.fedatamine.com/3h/face_school/339/0.png')
+					.addFields({ name: 'Edelgard Prediction 🦅', value: `${reply}` });
+				interaction.reply({ embeds: [embed] });
 			}
 		}
 
 		if (interaction.commandName === 'elhelp') {
 			// list all available commands
-			interaction.reply(`>>> **The following slash commands are available:**
-			**/elhey:** Edelgard will greet you back.
-			**/elquote:** Edelgard will reply with a random Edelgard quote.
-			**/elsight:** Edelgard will predict the future with the help of a 8ball. You should provide a question.
-			**/elhelp:** Edelgard will list all available commands. You just did this one. 🦅 
-			`);
+			const embed = new EmbedBuilder()
+				.setColor(0xff0000)
+				.setTimestamp()
+				.setThumbnail('https://assets.fedatamine.com/3h/face_school/16/0.png')
+				.addFields(
+					{ name: 'Edelgard Bot Slash Commands 🦅', value: `**The following slash commands are available**` },
+					{ name: '**/elhey**', value: 'Edelgard will greet you back.' },
+					{ name: '**/elquote**', value: 'Edelgard will reply with a random Edelgard quote.' },
+					{ name: '**/elsight**', value: 'Edelgard will predict the future with the help of a 8ball. You should provide a question.' },
+					{ name: '**/elhelp**', value: 'Edelgard will list all available commands. You just did this one. 🦅 ' },
+				);
+			interaction.reply({ embeds: [embed] });
 		}
 	}
 });
